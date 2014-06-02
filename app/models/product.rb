077dfb00c87e31e8a1d6aab8d593ba86cca30ecf
Tanner <dev.tannner@gmail.com>
2014-06-02 16:58:08 -0700

    Modified product validation messages

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ActiveRecord::Base
 
 	validates :title, :description, :image_url, presence: true
-	validates :price, numericality: {greater_than_or_equal_to: 0.01}
-	validates :title, uniqueness: true, length: {minimum: 10}
+	validates :price, numericality: {greater_than_or_equal_to: 0.01, message: "must be at least 1 cent (0.01)"}
+	validates :title, uniqueness: {message: "must be unique"}, length: {minimum: 10, message: "must be at least 10 characters long"}
 	validates :image_url, allow_blank: true, format: {
 		with:  /\.(gif|jpg|png)\z/i,
-		message: 'Must be a URL for gif, jpeg, or png image.'
+		message: 'must be a URL for gif, jpeg, or png image.'
 	}
 end
